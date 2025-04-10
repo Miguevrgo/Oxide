a75336891a919cb3afbb48a57b229dc95b69d16c
Miguel Angel De la Vega Rodríguez <93781730+Miguevrgo@users.noreply.github.com>
2025-04-10 15:13:47 +0200

    Delete target directory

--- target/release/build/lock_api-65b9c412c83106ac/build_script_build-65b9c412c83106ac.d
+++
@@ -1,5 +0,0 @@
-/home/miguevr/GitHub/Projects/Chess/chess/target/release/build/lock_api-65b9c412c83106ac/build_script_build-65b9c412c83106ac: /home/miguevr/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lock_api-0.4.12/build.rs
-
-/home/miguevr/GitHub/Projects/Chess/chess/target/release/build/lock_api-65b9c412c83106ac/build_script_build-65b9c412c83106ac.d: /home/miguevr/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lock_api-0.4.12/build.rs
-
-/home/miguevr/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lock_api-0.4.12/build.rs:
