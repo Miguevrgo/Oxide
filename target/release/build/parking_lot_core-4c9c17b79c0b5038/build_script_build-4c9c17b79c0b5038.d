e902d630d85883cae862bd29836c7d9b1e2e4de3
Miguevrgo <miguevrgo@gmail.com>
2025-04-10 14:53:34 +0200

    Move to a new repository

---
+++ target/release/build/parking_lot_core-4c9c17b79c0b5038/build_script_build-4c9c17b79c0b5038.d
@@ -0,0 +1,5 @@
+/home/miguevr/GitHub/Projects/Chess/chess/target/release/build/parking_lot_core-4c9c17b79c0b5038/build_script_build-4c9c17b79c0b5038: /home/miguevr/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/parking_lot_core-0.9.10/build.rs
+
+/home/miguevr/GitHub/Projects/Chess/chess/target/release/build/parking_lot_core-4c9c17b79c0b5038/build_script_build-4c9c17b79c0b5038.d: /home/miguevr/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/parking_lot_core-0.9.10/build.rs
+
+/home/miguevr/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/parking_lot_core-0.9.10/build.rs:
