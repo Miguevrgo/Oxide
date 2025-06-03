2bc9f1050cf51214bf100ea0d83350984e5ae6c0
Miguevrgo <miguevrgo@gmail.com>
2025-06-03 11:32:55 +0200

    Improve eval (Akimbo's style incremental updates)

--- src/engine/evaluation.rs
+++ src/engine/evaluation.rs
@@ -4,7 +4,7 @@ use crate::game::{
     piece::{Colour, Piece},
 };
 
-use super::network::{Accumulator, Network};
+use super::network::*;
 
 // Improved material values (centipawns)
 pub const PIECE_VALUES: [i32; 6] = [
@@ -16,59 +16,97 @@ pub const PIECE_VALUES: [i32; 6] = [
     20000, // King (not actually used in evaluation)
 ];
 
-pub fn evaluate(board: &Board) -> i32 {
+pub fn evaluate(board: &Board, cache: &mut EvalTable) -> i32 {
     let white_king_sq = board.king_square(Colour::White).index();
     let black_king_sq = board.king_square(Colour::Black).index();
 
-    let mut white_acc = Accumulator::default();
-    let mut black_acc = Accumulator::default();
+    let wbucket = Network::get_bucket::<0>(white_king_sq);
+    let bbucket = Network::get_bucket::<1>(black_king_sq);
 
-    fill_features(
+    let entry = &mut cache.table[wbucket][bbucket];
+
+    let mut addf = [[0; 32]; 2];
+    let mut subf = [[0; 32]; 2];
+    let (adds, subs) = fill_diff(
         board,
-        &mut white_acc,
-        &mut black_acc,
+        &entry.bbs,
+        &mut addf,
+        &mut subf,
         white_king_sq,
         black_king_sq,
     );
 
+    entry.white.update_multi(&addf[0][..adds], &subf[0][..subs]);
+    entry.black.update_multi(&addf[1][..adds], &subf[1][..subs]);
+
+    entry.bbs = [
+        board.sides[Colour::White as usize].0,
+        board.sides[Colour::Black as usize].0,
+        board.pieces[Piece::WP.index()].0,
+        board.pieces[Piece::WN.index()].0,
+        board.pieces[Piece::WB.index()].0,
+        board.pieces[Piece::WR.index()].0,
+        board.pieces[Piece::WQ.index()].0,
+        board.pieces[Piece::WK.index()].0,
+    ];
+
     let eval = if board.side == Colour::White {
-        Network::out(&white_acc, &black_acc)
+        Network::out(&entry.white, &entry.black)
     } else {
-        Network::out(&black_acc, &white_acc)
+        Network::out(&entry.black, &entry.white)
     };
 
     scale(board, eval)
 }
 
-fn fill_features(
+fn fill_diff(
     board: &Board,
-    white_acc: &mut Accumulator,
-    black_acc: &mut Accumulator,
+    bbs: &[u64; 8],
+    add_feats: &mut [[u16; 32]; 2],
+    sub_feats: &mut [[u16; 32]; 2],
     white_king_sq: usize,
     black_king_sq: usize,
-) {
+) -> (usize, usize) {
+    let mut adds = 0;
+    let mut subs = 0;
+
     let wflip = if white_king_sq % 8 > 3 { 7 } else { 0 };
     let bflip = if black_king_sq % 8 > 3 { 7 } else { 0 } ^ 56;
 
-    let occ = board.sides[Colour::White as usize] | board.sides[Colour::Black as usize];
-    let mut occupied = occ;
+    for side in [Colour::White as usize, Colour::Black as usize] {
+        let old_boys = bbs[side];
+        let new_boys = board.sides[side].0;
 
-    while occupied != BitBoard::EMPTY {
-        let sq = occupied.lsb();
-        if let Some(piece) = board.piece_at(sq) {
-            let side = piece.colour() as usize;
-            let pc = piece.index();
+        for (piece, &old_bb) in bbs[2..8].iter().enumerate() {
+            let old_bb = old_bb & old_boys;
+            let new_bb = board.pieces[piece].0 & new_boys;
 
-            let wbase = Network::get_base_index::<0>(side, pc, white_king_sq) as u16;
-            let bbase = Network::get_base_index::<1>(side, pc, black_king_sq) as u16;
-            let wfeat = wbase + (sq.index() as u16 ^ wflip);
-            let bfeat = bbase + (sq.index() as u16 ^ bflip);
+            let wbase = Network::get_base_index::<0>(side, piece, white_king_sq) as u16;
+            let bbase = Network::get_base_index::<1>(side, piece, black_king_sq) as u16;
 
-            white_acc.update_multi(&[wfeat]);
-            black_acc.update_multi(&[bfeat]);
+            let mut add_diff = BitBoard(new_bb & !old_bb);
+            while add_diff != BitBoard::EMPTY {
+                let sq = add_diff.lsb();
+                let sq_idx = sq.index() as u16;
+                add_feats[0][adds] = wbase + (sq_idx ^ wflip);
+                add_feats[1][adds] = bbase + (sq_idx ^ bflip);
+                adds += 1;
+                add_diff = add_diff.pop_bit(sq);
+            }
+
+            let mut sub_diff = BitBoard(old_bb & !new_bb);
+            while sub_diff != BitBoard::EMPTY {
+                let sq = sub_diff.lsb();
+                let sq_idx = sq.index() as u16;
+                sub_feats[0][subs] = wbase + (sq_idx ^ wflip);
+                sub_feats[1][subs] = bbase + (sq_idx ^ bflip);
+                subs += 1;
+                sub_diff = sub_diff.pop_bit(sq);
+            }
         }
-        occupied = occupied.pop_bit(sq);
     }
+
+    (adds, subs)
 }
 
 fn scale(board: &Board, eval: i32) -> i32 {
