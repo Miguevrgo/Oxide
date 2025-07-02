0ba800df937e667dbe48dfafccbd267e18e28a29
Miguevrgo <miguevrgo@gmail.com>
2025-07-02 17:47:32 +0200

    Refactor

--- src/engine/tables.rs
+++ src/engine/tables.rs
@@ -1,5 +1,5 @@
 use crate::game::moves::Move;
-use std::collections::HashMap;
+use std::{collections::HashMap, time::Instant};
 
 /// Transposition Table
 #[derive(Copy, Clone)]
@@ -18,7 +18,7 @@ pub struct TTEntry {
 }
 
 pub struct TranspositionTable {
-    tt: HashMap<u64, TTEntry>,
+    pub tt: HashMap<u64, TTEntry>,
 }
 
 impl TranspositionTable {
@@ -44,14 +44,58 @@ pub struct PlyData {
     pub killers: [Move; 2],
 }
 
-pub struct SearchTables {
-    pub ply: [PlyData; MAX_PLY],
+pub struct SearchData {
+    pub timing: Instant,
+    pub ply: usize,
+    pub nodes: u64,
+    pub stack: Vec<u64>,
+    pub ply_data: [PlyData; MAX_PLY],
+    pub tt: TranspositionTable,
 }
 
-impl SearchTables {
+impl SearchData {
     pub fn new() -> Self {
         Self {
-            ply: [(); MAX_PLY].map(|_| PlyData::default()),
+            timing: Instant::now(),
+            ply: 0,
+            nodes: 0,
+            stack: Vec::with_capacity(16),
+            ply_data: [(); MAX_PLY].map(|_| PlyData::default()),
+            tt: TranspositionTable::new(),
         }
+    }
+
+    pub fn push(&mut self, hash: u64) {
+        self.ply += 1;
+        self.stack.push(hash);
+    }
+
+    pub fn pop(&mut self) {
+        self.stack.pop();
+        self.ply -= 1;
+    }
+
+    pub fn clear(&mut self) {
+        self.stack.clear();
+        self.tt.tt.clear(); // TODO
+        self.nodes = 0;
+        self.ply = 0;
+    }
+
+    pub fn is_repetition(&self, curr_hash: u64, root: bool) -> bool {
+        if self.stack.len() < 6 {
+            return false;
+        }
+
+        let mut reps = 1 + u8::from(root);
+        for &hash in self.stack.iter().rev().skip(1).step_by(2) {
+            if hash == curr_hash {
+                reps -= 1;
+                if reps == 0 {
+                    return true;
+                }
+            }
+        }
+        false
     }
 }
