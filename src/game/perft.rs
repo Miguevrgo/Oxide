9ce8ae9461d293b23a1a5069ab7981fe440762d6
Miguevrgo <miguevrgo@gmail.com>
2025-07-22 10:15:49 +0200

    Upload perft tests, decrease NUM_THREADS for greater stability in results

--- src/game/perft.rs
+++ src/game/perft.rs
@@ -8,7 +8,7 @@ use crate::game::moves::Move;
 pub const BULK: bool = true;
 #[allow(unused)]
 pub const NO_BULK: bool = false;
-pub const NUM_THREADS: usize = 16;
+pub const NUM_THREADS: usize = 8;
 
 #[allow(unused)]
 pub fn perft_with_moves(board: &mut Board, depth: usize) -> u64 {
@@ -166,98 +166,28 @@ mod tests {
 
     #[test]
     fn test_perft_suite() {
-        const PERFT_SUITE: [(&str, &str, u64, usize); 16] = [
-            (
-                "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
-                "Startpos",
-                119_060_324,
-                6,
-            ),
-            (
-                "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
-                "Kiwipete",
-                193_690_690,
-                5,
-            ),
-            (
-                "8/8/4k3/8/2p5/8/B2P2K1/8 w - - 0 1",
-                "Illegal ep move #1",
-                1_015_133,
-                6,
-            ),
-            (
-                "3k4/3p4/8/K1P4r/8/8/8/8 b - - 0 1",
-                "Illegal ep move #2",
-                1_134_888,
-                6,
-            ),
-            (
-                "8/8/1k6/2b5/2pP4/8/5K2/8 b - d3 0 1",
-                "Ep capture checks opponent",
-                1_440_467,
-                6,
-            ),
-            (
-                "5k2/8/8/8/8/8/8/4K2R w K - 0 1",
-                "Short castling gives check",
-                661_072,
-                6,
-            ),
-            (
-                "3k4/8/8/8/8/8/8/R3K3 w Q - 0 1",
-                "Long castling gives check",
-                803_711,
-                6,
-            ),
-            (
-                "r3k2r/1b4bq/8/8/8/8/7B/R3K2R w KQkq - 0 1",
-                "Castle rights",
-                1_274_206,
-                4,
-            ),
-            (
-                "r3k2r/8/3Q4/8/8/5q2/8/R3K2R b KQkq - 0 1",
-                "Castling prevented",
-                1_720_476,
-                4,
-            ),
-            (
-                "2K2r2/4P3/8/8/8/8/8/3k4 w - - 0 1",
-                "Promote out of check",
-                3_821_001,
-                6,
-            ),
-            (
-                "8/8/1P2K3/8/2n5/1q6/8/5k2 b - - 0 1",
-                "Discovered check",
-                1_004_658,
-                5,
-            ),
-            (
-                "4k3/1P6/8/8/8/8/K7/8 w - - 0 1",
-                "Promote to give check",
-                217_342,
-                6,
-            ),
-            (
-                "8/P1k5/K7/8/8/8/8/8 w - - 0 1",
-                "Under promote to give check",
-                92_683,
-                6,
-            ),
-            ("K1k5/8/P7/8/8/8/8/8 w - - 0 1", "Self stalemate", 2_217, 6),
-            (
-                "8/k1P5/8/1K6/8/8/8/8 w - - 0 1",
-                "Stalemate & checkmate #1",
-                567_584,
-                7,
-            ),
-            (
-                "8/8/2k5/5q2/5n2/8/5K2/8 b - - 0 1",
-                "Stalemate & checkmate #2",
-                23_527,
-                4,
-            ),
+        #[rustfmt::skip]
+        const PERFT_SUITE: [(&str, &str, u64, usize); 19] = [
+            ("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", "Startpos", 119060324, 6),
+            ("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", "Kiwipete", 193690690, 5),
+            ("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1", "Rook and pawns Pos 3 CPW", 11030083, 6),
+            ("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1", "Pos 4 CPW", 15833292, 5),
+            ("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8", "Pos 5 CPW", 89941194, 5),
+            ("8/8/4k3/8/2p5/8/B2P2K1/8 w - - 0 1", "Illegal ep move #1", 14047573, 7),
+            ("3k4/3p4/8/K1P4r/8/8/8/8 b - - 0 1", "Illegal ep move #2", 20757544, 7),
+            ("8/8/1k6/2b5/2pP4/8/5K2/8 b - d3 0 1", "Ep capture checks opponent", 21190412, 7),
+            ("5k2/8/8/8/8/8/8/4K2R w K - 0 1", "Short castling gives check", 12762196, 7),
+            ("3k4/8/8/8/8/8/8/R3K3 w Q - 0 1", "Long castling gives check", 91628014, 8),
+            ("r3k2r/1b4bq/8/8/8/8/7B/R3K2R w KQkq - 0 1", "Castle rights", 31912360, 5),
+            ("r3k2r/8/3Q4/8/8/5q2/8/R3K2R b KQkq - 0 1", "Castling prevented", 58773923, 5),
+            ("2K2r2/4P3/8/8/8/8/8/3k4 w - - 0 1", "Promote out of check", 60651209, 7),
+            ("8/8/1P2K3/8/2n5/1q6/8/5k2 b - - 0 1", "Discovered check", 6334638, 6),
+            ("4k3/1P6/8/8/8/8/K7/8 w - - 0 1", "Promote to give check", 217342, 6),
+            ("8/P1k5/K7/8/8/8/8/8 w - - 0 1", "Under promote to give check", 8110830, 8),
+            ("K1k5/8/P7/8/8/8/8/8 w - - 0 1", "Self stalemate", 5966690, 10),
+            ("8/k1P5/8/1K6/8/8/8/8 w - - 0 1", "Stalemate & checkmate #1", 2518905, 8),
+            ("8/8/2k5/5q2/5n2/8/5K2/8 b - - 0 1", "Stalemate & checkmate #2", 3114998, 6),
+
         ];
 
         let mut failures = Vec::new();
